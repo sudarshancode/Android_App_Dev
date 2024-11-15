b548cb68f29b149fb4aa4fcb09257cef41205ab7
sudarshan chakraborty <sudarshanchakraborty420@gmail.com>
2024-11-15 23:08:33 +0530

    Add BottomNavigation

---
+++ BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.bottomnavigation;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.bottomnavigation.Fragment.CallFragment;
+import com.example.bottomnavigation.Fragment.ChatFragment;
+import com.example.bottomnavigation.Fragment.updateFragment;
+import com.example.bottomnavigation.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        setContentView(binding.getRoot());
+
+        replaceFragment(new ChatFragment());
+
+        binding.bottomNavMenuId.setOnItemSelectedListener(item -> {
+
+            if(item.getItemId() == R.id.chat){
+                replaceFragment(new ChatFragment());
+            }
+            if(item.getItemId()==R.id.update){
+                replaceFragment(new updateFragment());
+            }
+            if(item.getItemId()==R.id.call){
+                replaceFragment(new CallFragment());
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_layout,fragment);
+        fragmentTransaction.commit();
+    }
+}
